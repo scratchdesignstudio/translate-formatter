7e37dcd759be58d885aafa79c07efc9948ed6339
The_Grits <gritsprogramming@gmail.com>
2016-03-05 22:48:10 -0500

    Dynamic changing test

--- formatter.js
+++ formatter.js
@@ -1,1 +1,13 @@
+var link, age, appropriate, patience, whatIsSDS, result;
+window.onload = function(){
+  link = document.getElementById('link');
+  age = document.getElementById('age');
+  appropriate = document.getElementById('appropriate');
+  patience = document.getElementById('patience');
+  whatIsSDS = document.getElementById('whatisSDS');
+  result = document.getElementById('result');
+};
 
+window.onKeyUp = function(){
+  result.innerHTML = link.value + "\n" + "<br/>" + age.value + "\n" + "<br/>" + appropriate.value + "\n" + "<br/>" + patience.value + "\n" + "<br/><br/>"; 
+};
