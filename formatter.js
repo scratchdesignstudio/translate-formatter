0408a5d72508879803b8a7423db9f45883e43a28
The_Grits <gritsprogramming@gmail.com>
2016-03-07 10:29:48 -0500

    oops I skipped the wiki

--- formatter.js
+++ formatter.js
@@ -8,11 +8,12 @@ window.onload = function(){
   selected = document.getElementById('selected');
   ask = document.getElementById('ask');
   idea = document.getElementById('idea');
+  wiki = document.getElementById('wiki');
   account = document.getElementById('account');
   thumbnail = document.getElementById('thumbnail');
   result = document.getElementById('result');
 };
 
 document.onkeyup = function(){
-  result.innerHTML = link.value + "\\n" + "&lt;br/&gt;" + age.value + "\\n" + "&lt;br/&gt;" + appropriate.value + "\\n" + "&lt;br/&gt;" + patience.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + whatIsSDS.value + "\\n" + selected.value + "\\n" + ask.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + idea.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + account.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + thumbnail.value; 
+  result.innerHTML = link.value + "\\n" + "&lt;br/&gt;" + age.value + "\\n" + "&lt;br/&gt;" + appropriate.value + "\\n" + "&lt;br/&gt;" + patience.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + whatIsSDS.value + "\\n" + selected.value + "\\n" + ask.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + idea.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + wiki.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + account.value + "\\n" + "&lt;br/&gt;&lt;br/&gt;" + thumbnail.value; 
 };
