5023d8f9364b2df4b931a604de61bd41f4050a3b
The_Grits <gritsprogramming@gmail.com>
2016-03-07 17:31:54 -0500

    regex to escape quotes
    Added a regex that escapes quotes within the translation.

--- formatter.js
+++ formatter.js
@@ -1,16 +1,16 @@
 var link, age, appropriate, patience, whatIsSDS, result;
 document.onkeyup = function(){
-  link = document.getElementById('link').value + "\\n" + "&lt;br/&gt;";
-  age = document.getElementById('age').value + "\\n" + "&lt;br/&gt;";
-  appropriate = document.getElementById('appropriate').value + "\\n" + "&lt;br/&gt;";
-  patience = document.getElementById('patience').value + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
-  whatIsSDS = document.getElementById('whatIsSDS').value + "\\n";
-  selected = " " + document.getElementById('selected').value + " &lt;a href=\"https://scratch.mit.edu/projects/55738732/\"&gt;" + "https://scratch.mit.edu/projects/55738732/" + "&lt;/a&gt;" + "\\n";
-  ask = document.getElementById('ask').value + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
-  idea = document.getElementById('idea').value + " &lt;a href=\"https://scratch.mit.edu/studios/93627/\"&gt;" + "https://scratch.mit.edu/studios/93627/" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
-  wiki = document.getElementById('wiki').value + " &lt;a href=\"http://wiki.scratch.mit.edu/wiki/SDS/\"&gt;" + "http://wiki.scratch.mit.edu/wiki/SDS/" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
-  account = document.getElementById('account').value +  " &lt;a href=\"https://scratch.mit.edu/users/SDS-Updates/\"&gt;" + "@SDS-Updates" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
-  thumbnail = document.getElementById('thumbnail').value;
+  link = document.getElementById('link').value.replace(/[\"]/g, "\\\""); + "\\n" + "&lt;br/&gt;";
+  age = document.getElementById('age').value.replace(/[\"]/g, "\\\""); + "\\n" + "&lt;br/&gt;";
+  appropriate = document.getElementById('appropriate').value.replace(/[\"]/g, "\\\""); + "\\n" + "&lt;br/&gt;";
+  patience = document.getElementById('patience').value.replace(/[\"]/g, "\\\""); + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
+  whatIsSDS = document.getElementById('whatIsSDS').value.replace(/[\"]/g, "\\\""); + "\\n";
+  selected = " " + document.getElementById('selected').value.replace(/[\"]/g, "\\\""); + " &lt;a href=\"https://scratch.mit.edu/projects/55738732/\"&gt;" + "https://scratch.mit.edu/projects/55738732/" + "&lt;/a&gt;" + "\\n";
+  ask = document.getElementById('ask').value.replace(/[\"]/g, "\\\""); + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
+  idea = document.getElementById('idea').value.replace(/[\"]/g, "\\\""); + " &lt;a href=\"https://scratch.mit.edu/studios/93627/\"&gt;" + "https://scratch.mit.edu/studios/93627/" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
+  wiki = document.getElementById('wiki').value.replace(/[\"]/g, "\\\""); + " &lt;a href=\"http://wiki.scratch.mit.edu/wiki/SDS/\"&gt;" + "http://wiki.scratch.mit.edu/wiki/SDS/" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
+  account = document.getElementById('account').value.replace(/[\"]/g, "\\\""); +  " &lt;a href=\"https://scratch.mit.edu/users/SDS-Updates/\"&gt;" + "@SDS-Updates" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
+  thumbnail = document.getElementById('thumbnail').value.replace(/[\"]/g, "\\\"");;
   result = document.getElementById('result');
   result.innerHTML = link + age + appropriate + patience + whatIsSDS + selected + ask + idea + wiki + account + thumbnail;
 };
