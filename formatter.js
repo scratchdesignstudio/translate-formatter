ec9967bf44aeb967c4b75b36b08b0575d7b287f4
The_Grits <gritsprogramming@gmail.com>
2016-03-07 16:18:43 -0500

    Finish links

--- formatter.js
+++ formatter.js
@@ -7,9 +7,9 @@ document.onkeyup = function(){
   whatIsSDS = document.getElementById('whatIsSDS').value + "\\n";
   selected = document.getElementById('selected').value + " &lt;a href=\"https://scratch.mit.edu/projects/55738732/\"&gt;" + "https://scratch.mit.edu/projects/55738732/" + "&lt;/a&gt;" + "\\n";
   ask = document.getElementById('ask').value + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
-  idea = document.getElementById('idea').value + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
-  wiki = document.getElementById('wiki').value + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
-  account = document.getElementById('account').value + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
+  idea = document.getElementById('idea').value + " &lt;a href=\"https://scratch.mit.edu/studios/93627/\"&gt;" + "https://scratch.mit.edu/studios/93627/" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
+  wiki = document.getElementById('wiki').value + " &lt;a href=\"http://wiki.scratch.mit.edu/wiki/SDS/\"&gt;" + "http://wiki.scratch.mit.edu/wiki/SDS/" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
+  account = document.getElementById('account').value +  " &lt;a href=\"https://scratch.mit.edu/users/SDS-Updates/\"&gt;" + "@SDS-Updates" + "&lt;/a&gt;" + "\\n" + "&lt;br/&gt;&lt;br/&gt;";
   thumbnail = document.getElementById('thumbnail').value;
   result = document.getElementById('result');
   result.innerHTML = link + age + appropriate + patience + whatIsSDS + selected + ask + idea + wiki + account + thumbnail;
