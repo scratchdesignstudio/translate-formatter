e1cc52684bcb721bd9c7b359147f6fb9939fb547
The_Grits <gritsprogramming@gmail.com>
2016-04-20 09:59:30 -0400

    uh oh

--- formatter.js
+++ formatter.js
@@ -21,25 +21,25 @@ var render = function(){
   final[lang] = {};
   final[lang]['language'] = dropdown.selectedOptions[0].text;
 
-  final[lang]["translator"] = document.getElementById('translator').value.replace(/[\"]/g, "\\"");
-  var unique1 = document.getElementById('unique1').value.replace(/[\"]/g, "\\"") + "\n" + "<br/><br/>";
-  var unique2 = document.getElementById('unique2').value.replace(/[\"]/g, "\\"") + "<br/>";
+  final[lang]["translator"] = document.getElementById('translator').value.replace(/[\"]/g, "\\\"");
+  var unique1 = document.getElementById('unique1').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/><br/>";
+  var unique2 = document.getElementById('unique2').value.replace(/[\"]/g, "\\\"") + "<br/>";
   final[lang]["unique"] = unique1 + unique2;
 
-  var link = document.getElementById('link').value.replace(/[\"]/g, "\\"") + "\n" + "<br/>";
-  var age = document.getElementById('age').value.replace(/[\"]/g, "\\"") + "\n" + "<br/>";
-  var appropriate = document.getElementById('appropriate').value.replace(/[\"]/g, "\\"") + "\n" + "<br/>";
-  var patience = document.getElementById('patience').value.replace(/[\"]/g, "\\"") + "\n" + "<br/><br/>";
-  var whatIsSDS = document.getElementById('whatIsSDS').value.replace(/[\"]/g, "\\"") + "\n";
-  var selected = " " + document.getElementById('selected').value.replace(/[\"]/g, "\\"") + " <a href=\"https://scratch.mit.edu/projects/55738732/\">" + "https://scratch.mit.edu/projects/55738732/" + "</a>" + "\n";
-  var ask = document.getElementById('ask').value.replace(/[\"]/g, "\\"") + "\n" + "<br/><br/>";
-  var idea = document.getElementById('idea').value.replace(/[\"]/g, "\\"") + " <a href=\"https://scratch.mit.edu/studios/93627/\">" + "https://scratch.mit.edu/studios/93627/" + "</a>" + "\n" + "<br/><br/>";
-  var wiki = document.getElementById('wiki').value.replace(/[\"]/g, "\\"") + " <a href=\"http://wiki.scratch.mit.edu/wiki/SDS/\">" + "http://wiki.scratch.mit.edu/wiki/SDS/" + "</a>" + "\n" + "<br/><br/>";
-  var account = document.getElementById('account').value.replace(/[\"]/g, "\\"") +  " <a href=\"https://scratch.mit.edu/users/SDS-Updates/\">" + "@SDS-Updates" + "</a>" + "\n" + "<br/><br/>";
-  var thumbnail = document.getElementById('thumbnail').value.replace(/[\"]/g, "\\"");
+  var link = document.getElementById('link').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/>";
+  var age = document.getElementById('age').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/>";
+  var appropriate = document.getElementById('appropriate').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/>";
+  var patience = document.getElementById('patience').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/><br/>";
+  var whatIsSDS = document.getElementById('whatIsSDS').value.replace(/[\"]/g, "\\\"") + "\n";
+  var selected = " " + document.getElementById('selected').value.replace(/[\"]/g, "\\\"") + " <a href=\"https://scratch.mit.edu/projects/55738732/\">" + "https://scratch.mit.edu/projects/55738732/" + "</a>" + "\n";
+  var ask = document.getElementById('ask').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/><br/>";
+  var idea = document.getElementById('idea').value.replace(/[\"]/g, "\\\"") + " <a href=\"https://scratch.mit.edu/studios/93627/\">" + "https://scratch.mit.edu/studios/93627/" + "</a>" + "\n" + "<br/><br/>";
+  var wiki = document.getElementById('wiki').value.replace(/[\"]/g, "\\\"") + " <a href=\"http://wiki.scratch.mit.edu/wiki/SDS/\">" + "http://wiki.scratch.mit.edu/wiki/SDS/" + "</a>" + "\n" + "<br/><br/>";
+  var account = document.getElementById('account').value.replace(/[\"]/g, "\\\"") +  " <a href=\"https://scratch.mit.edu/users/SDS-Updates/\">" + "@SDS-Updates" + "</a>" + "\n" + "<br/><br/>";
+  var thumbnail = document.getElementById('thumbnail').value.replace(/[\"]/g, "\\\"");
   final[lang]["standard"] = link + age + appropriate + patience + whatIsSDS + selected + ask + idea + wiki + account + thumbnail;
 
-  var curators = document.getElementById('curatorsp').value.replace(/[\"]/g, "\\"");
+  var curators = document.getElementById('curatorsp').value.replace(/[\"]/g, "\\\"");
   var result = document.getElementById('result');
   final[lang]["curators"] = curators;
 
