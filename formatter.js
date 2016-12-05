1eca91883e1faa74937a09c8771e1011e5d03c00
The_Grits <gritsprogramming@gmail.com>
2016-12-05 07:31:19 -0500

    Update Uniques

--- formatter.js
+++ formatter.js
@@ -23,9 +23,8 @@ var render = function(){
 
   final[lang]["translator"] = document.getElementById('translator').value.replace(/[\"]/g, "\\\"");
   var unique1 = document.getElementById('unique1').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/><br/>";
-  var unique2 = document.getElementById('unique2').value.replace(/[\"]/g, "\\\"") + "<br/><br/>";
-  var unique3 = document.getElementById('unique3').value.replace(/[\"]/g, "\\\"") + "<br/>";
-  final[lang]["unique"] = unique1 + unique2 + unique3;
+  var unique2 = document.getElementById('unique2').value.replace(/[\"]/g, "\\\"") + "<br/>";
+  final[lang]["unique"] = unique1 + unique2;
 
   var link = document.getElementById('link').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/>";
   var age = document.getElementById('age').value.replace(/[\"]/g, "\\\"") + "\n" + "<br/>";
